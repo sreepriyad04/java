43060e62590ba90bb008719fb9c3127b5d9a29f2
sreepriyad04 <chatgptmail1104@gmail.com>
2026-07-22 22:37:53 +0530

    updated code
    Updated the main class to demonstrate shallow and deep copy of PersonClass instances.

--- learningjava/src/PersonClassDeepCpoy/MainClass.java
+++ learningjava/src/PersonClassDeepCpoy/MainClass.java
@@ -5,17 +5,31 @@ public class MainClass {
 	public static void main(String args[]) {
 		PersonClass p1 =  new PersonClass(10, "Kim");
 		
-		//PersonClass p =  new PersonClass();
-		//p.name="p.Name";
-		
-		System.out.println(p1.printMessage());
-	//	System.out.println(p.toString());
-		
 		//for deep copy create a new object and assign all the old oject values to new one 
 		
-		PersonClass p2 = new PersonClass(0, null);
-		p2.age=p1.age;
-		p2.name=p1.name;
-		System.out.println(p2.printMessage());
+		PersonClass p2 = p1; //shallow copy
+		p2.age=30;
+		//p2.name=p1.name;
+		System.out.println("Shallow copy ");
+		System.out.println("p2=="+p2.printMessage());
+		System.out.println("p1=="+p1.printMessage());
+		
+		// for deep copy
+		
+		System.out.println("Deep copy ");
+		PersonClass p3= new PersonClass(40,"deepCopy");
+		PersonClass p4= new PersonClass(p3);
+		
+		System.out.println("before assigning new values ");
+		System.out.println(p3.printMessage());
+		System.out.println(p4.printMessage()); // empty object
+		
+		
+        p4.age=50;
+        p4.name="after deep copy";
+        System.out.println("after assigning deep copy values");
+        System.out.println(p3.printMessage());// prints p3 object detalils
+		System.out.println(p4.printMessage()); // prints updated p4 details
+		
 	}
 }
